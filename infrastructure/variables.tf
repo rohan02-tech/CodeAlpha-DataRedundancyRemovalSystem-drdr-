cbaa79db28be5a31d15a221e06e3fd402e26eef3
rohan02-tech <rohanraj7999@gmail.com>
2025-08-28 16:13:09 +0530

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,1 +1,34 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
 
+variable "region" {
+  description = "The GCP region for resources"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "firestore_region" {
+  description = "The region for Firestore"
+  type        = string
+  default     = "nam5"
+}
+
+variable "spanner_nodes" {
+  description = "Number of Spanner nodes"
+  type        = number
+  default     = 1
+}
+
+variable "force_destroy" {
+  description = "Force destroy bucket with contents"
+  type        = bool
+  default     = false
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "development"
+}
