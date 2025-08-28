fddd89ebeea1637f4ef4631048d70203fa9425bc
rohan02-tech <rohanraj7999@gmail.com>
2025-08-28 17:22:05 +0530

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -24,7 +24,7 @@ variable "spanner_nodes" {
 variable "force_destroy" {
   description = "Force destroy bucket with contents"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "environment" {
